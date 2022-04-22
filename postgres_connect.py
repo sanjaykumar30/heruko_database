45ecd039c54deb9f7f6c66a61a7011aade884cec
sanjaykumar30 <m.sanjaykumar3072002@gamil.com>
2022-04-23 00:40:47 +0530

    update main.py

--- postgres_connect.py
+++ postgres_connect.py
@@ -1,8 +1,8 @@
-import psycopg2
+from psycopg2 import *
 
 def runquery(sql):
     try:
-        connection=psycopg2.connect(user="ofbqtxxmxboxbm",password="b599b5cc38b5878b8ed21e41e1c4174c25c756ef8a404dd6da5c8dfa4815be84",host="ec2-52-203-118-49.compute-1.amazonaws.com",database="dcsudgtuo5s500")
+        connection=connect(user="ofbqtxxmxboxbm",password="b599b5cc38b5878b8ed21e41e1c4174c25c756ef8a404dd6da5c8dfa4815be84",host="ec2-52-203-118-49.compute-1.amazonaws.com",database="dcsudgtuo5s500")
         cursor=connection.cursor()
         cursor.execute(sql)
         record=cursor.fetchall()
