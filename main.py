94eb8f46c77ba42a38f9eb0093d0b9a46aaa4b94
sanjaykumar30 <m.sanjaykumar3072002@gamil.com>
2022-04-23 00:20:17 +0530

    update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import altair as alt
-from postgres_connect import *
-
+import psycopg2
+import postgres_connect
 sql="select * from data_training_2.cars"
-data=pd.DataFrame(runquery(sql))
+data=pd.DataFrame(postgres_connect.runquery(sql))
 data.columns=["name","miles_per_gallon","cylinders","displacement","horsepower","weight_in_lbs","acceleration","year","origin"]
 st.title("CARS DATA SOURCED FROM - HERUKO")
 st.table(data)
